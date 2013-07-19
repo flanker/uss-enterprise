7bd667c790ccacd4930f43b78babd2cc2d07fb00
FENG Zhichao <flankerfc@gmail.com>
2013-07-19 19:23:23 +1000

    add illustration layer.

--- enterprise.js
+++ enterprise.js
@@ -1,16 +1,31 @@
 $(function () {
+
   var foreground = $('<div class="foreground slides"></div>');
+  var middle = $('<div class="middle slides"></div>');
+
   $('.slide').each(function () {
-    var originalSection = $(this);
-    var children = $(originalSection).children();
-    var newSection = $('<section class="slide"></section>').append(children);
-    foreground.append(newSection);
+
+    var $originalSection = $(this);
+
+    var illustration = $originalSection.find('.illustration');
+    var newMiddleSection = $('<section class="slide"></section>').append(illustration);
+
+    var content = $originalSection.children();
+    var newForegroundSection = $('<section class="slide"></section>').append(content);
+
+    foreground.append(newForegroundSection);
+    middle.append(newMiddleSection);
+
   });
+
+  $('.page').append(middle);
   $('.page').append(foreground);
+
 });
 
 $(window).scroll(function () {
   var scrollTop = $(window).scrollTop();
   $('.scrollTop').text(scrollTop);
   $('.foreground').css('top', -scrollTop);
+  $('.middle').css('top', -scrollTop * 3);
 });
